b52d9202d069cfafaa05b6757da19c93be087fb7
Wishvanath SMG <anandwishvanath@gmail.com>
2020-01-27 20:52:02 +0530

    Added Route and created todo list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
-
+import React, { Component, Fragment } from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Home from './components/home'
+import Edit from './components/edit'
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>This is App component</h1>
-
-      </div>
+      <Fragment>
+        <Switch>
+          <Route exact path ="/" component ={Home} />
+          <Route exact path ="/edit" component ={Edit} />
+        </Switch>
+      </Fragment>
     )
   }
 }
